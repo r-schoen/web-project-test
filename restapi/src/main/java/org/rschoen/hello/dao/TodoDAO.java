6f485d2eafd6ce1d96622bff745a80595b3ed565
Russell Schoen <rschoen@oreillyauto.com>
2019-11-26 08:58:26 -0600

    Removed build and .gradle folders

--- restapi/src/main/java/org/rschoen/hello/dao/TodoDAO.java
+++ restapi/src/main/java/org/rschoen/hello/dao/TodoDAO.java
@@ -8,15 +8,17 @@ import java.util.Date;
 
 @Repository
 public class TodoDAO {
+    public static ArrayList<Todo> todoList;
+
+    public TodoDAO() {
+        // TODO: Hook this up to an actual database
+        todoList = new ArrayList<Todo>();
+        todoList.add(new Todo(new Date(), "Study Chinese"));
+        todoList.add(new Todo(new Date(), "Take out trash"));
+        todoList.add(new Todo(new Date(), "Pickup Eggs"));
+    }
 
     public ArrayList<Todo> fetchTodos() {
-        // TODO: Hook this up to an actual database
-        // to solve the port to port on the same server problem:
-        // * https://stackoverflow.com/questions/57969766/not-able-to-set-proxy-port-in-my-vue-js-app
-        ArrayList<Todo> schtuff = new ArrayList<>();
-        schtuff.add(new Todo(new Date(), "Study Chinese"));
-        schtuff.add(new Todo(new Date(), "Take out trash"));
-        schtuff.add(new Todo(new Date(), "Pickup Eggs"));
-        return schtuff;
+        return todoList;
     }
 }
---
+++ restapi/src/test/groovy/org/rschoen/hello/service/GreetingServiceSpec.groovy
@@ -0,0 +1,14 @@
+package org.rschoen.hello.service
+
+import org.rschoen.hello.service.GreetingService
+
+public class GreetingServiceSpec extends spock.lang.Specification {
+    def "Saying Hello"() {
+        when: "The greeting Class is created"
+        def greeter = new GreetingService();
+
+
+        then: "The greeting should be correct"
+        greeter.Greeting("world").getContent() == "Hello, world!"
+    }
+}
